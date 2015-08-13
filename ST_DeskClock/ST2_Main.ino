072fa6018ce571bee0456b9a609650dd159161b5
Roy Vanegas <roy@thecodeeducators.com>
2015-08-13 08:20:31 -0400

    Format file “ST2_Main.ino”

--- ST_DeskClock/ST2_Main.ino
+++ ST_DeskClock/ST2_Main.ino
@@ -5,17 +5,14 @@
  */
 
 // Test for Sleep
-static void
-check_sleep_timer(void)
+static void check_sleep_timer(void)
 {
    currentMillis = millis();
    OptionModeFlag = false;
 
-   if(SleepEnable && (currentMillis - SleepTimer) > SleepLimit)
-   {
+   if (SleepEnable && (currentMillis - SleepTimer) > SleepLimit) {
       // New for ST Desk Clock - goto Time vs Sleep
-      if (STATE== 1)
-      {
+      if (STATE == 1) {
          SUBSTATE = 1;
          blinkON = true;
          blinkFlag = false;
@@ -31,40 +28,37 @@ check_sleep_timer(void)
    }
 }
 
-
 // Test for Mode Button Press
-static void
-check_mode_button(void)
+static void check_mode_button(void)
 {
-   if (digitalRead(MODEBUTTON))
+   if (digitalRead(MODEBUTTON)) {
       return;
+   }
 
-   if(ALARMON)
+   if (ALARMON) {
       CheckAlarm();
+   }
 
-   if(ALARM1FLAG)
-   {
+   if (ALARM1FLAG) {
       ALARM1FLAG = false;
       ALARMON = false;
       EnableAlarm1(false);
       STATE = 90;
       JustWokeUpFlag = false;
-   } else
-   if(JustWokeUpFlag)
-   {
-      // Used to supress "Time" text from showing when waking up.
-      JustWokeUpFlag = false;
-      JustWokeUpFlag2 = true;
    } else {
-      NextStateRequest = true;
-      clearmatrix();
+      if (JustWokeUpFlag) {
+         // Used to supress "Time" text from showing when waking up.
+         JustWokeUpFlag = false;
+         JustWokeUpFlag2 = true;
+      } else {
+         NextStateRequest = true;
+         clearmatrix();
+      }
    }
 
-   while(1)
-   {
+   while (1) {
       // check for simultaneous mode and set buttons
-      if (!digitalRead(SETBUTTON))
-      {
+      if (!digitalRead(SETBUTTON)) {
          OptionModeFlag = true;
          NextStateRequest = false;
          NextSUBStateRequest = false;
@@ -73,34 +67,33 @@ check_mode_button(void)
       }
 
       // wait for them to stop holding the button
-      if (!digitalRead(MODEBUTTON))
+      if (!digitalRead(MODEBUTTON)) {
          break;
+      }
    }
 
    delay(100);
    SleepTimer = millis();
 }
 
-
 // Test for SET Button Press
-static void
-check_set_button(void)
+static void check_set_button(void)
 {
-   if (digitalRead(SETBUTTON))
+   if (digitalRead(SETBUTTON)) {
       return;
+   }
 
    // if the mode button is held down as well, do nothing
-   if (OptionModeFlag)
+   if (OptionModeFlag) {
       return;
+   }
 
    NextSUBStateRequest = true;
    clearmatrix();
 
-   while(1)
-   {
+   while (1) {
       // this is repeated from above; can be merged?
-      if (!digitalRead(MODEBUTTON))
-      {
+      if (!digitalRead(MODEBUTTON)) {
          OptionModeFlag = true;
          NextStateRequest = false;
          NextSUBStateRequest = false;
@@ -109,79 +102,81 @@ check_set_button(void)
       }
 
       // wait for them to stop holding the button
-      if (digitalRead(SETBUTTON))
+      if (digitalRead(SETBUTTON)) {
          break;
+      }
    }
 
    delay(100);
    SleepTimer = millis();
 }
 
-
 // Running Blink counter
-static void
-check_blink(void)
+static void check_blink(void)
 {
-   if(!blinkFlag)
-   {
+   if (!blinkFlag) {
       // Not blinking, just leave the LEDs lit
       blinkON = true;
+
       return;
    }
 
    blinkCounter++;
-   if (blinkCounter >blinkTime) // was 150
-   {
+
+   if (blinkCounter >blinkTime) { // was 150
       blinkON = !blinkON;
       blinkCounter = 0;
    }
 }
 
-
-void loop()
-{
+void loop() {
    check_sleep_timer();
    check_mode_button();
    check_set_button();
    check_blink();
 
    // these should be moved into function pointers.
-   switch (STATE)
-   {
+   switch (STATE) {
    case 0: // Set-Up
       STATE = 1;
+
       break;
 
    case 1: // Display Time
       DisplayTimeSub();
+
       break;
 
    case 2: // Set Time
       setTimeSub();
+
       break;
 
    case 3: // Config Alarm
       setAlarmSub();
+
       break;
 
    case 4: // Stop Watch
       StopWatch();
+
       break;
 
    case 5: // Serial Display
       DisplaySerialData();
+
       break;
 
    case 6: // Graphic Demo
       graphican();
+
       break;
 
    case 90: // Alarm Triggered
       blinkFlag = true;
       displayString("Beep");
 
-      if(blinkON)
-      {
+      if (blinkON) {
          pinMode(SETBUTTON, OUTPUT);
          tone(SETBUTTON,4000) ;
          delay(100);
@@ -190,14 +185,14 @@ void loop()
       }
 
 #if ARDUINO >= 101
-      pinMode(SETBUTTON, INPUT_PULLUP);
+   pinMode(SETBUTTON, INPUT_PULLUP);
 #else
-      pinMode(SETBUTTON, INPUT);
+   pinMode(SETBUTTON, INPUT);
 #endif
+
       delay(250);
 
-      if(NextSUBStateRequest || NextStateRequest)
-      {
+      if (NextSUBStateRequest || NextStateRequest) {
          STATE = 0;
          SUBSTATE = 0;
          // NextStateFlag = true;
@@ -205,6 +200,7 @@ void loop()
          NextSUBStateRequest = false;
          blinkFlag = false;
       }
+
       break;
 
    case 99: // Sleep
@@ -215,6 +211,7 @@ void loop()
       SleepTimer = millis();
       STATE = 0;
       SUBSTATE = 0;
+
       break;
    }
 }
