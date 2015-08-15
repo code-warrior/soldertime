deba09b3472e8c5957bd2b5be6721f4423114d1b
Roy Vanegas <roy@thecodeeducators.com>
2015-08-15 11:23:16 -0400

    Format function “TwelveTwentyFourConvert”

--- ST_DeskClock/ST2_RTC.ino
+++ ST_DeskClock/ST2_RTC.ino
@@ -411,86 +411,74 @@ void set_alarm(uint8_t setselect)
  * ********************************************************************************/
 void TwelveTwentyFourConvert()
 {
+   int temphours = 0;
+   int temp = 0;
 
-  int temphours = 0;
-  int temp = 0;
+   I2C_RX(RTCDS1337, RTC_HOUR);
 
-  I2C_RX(RTCDS1337,RTC_HOUR);
-  HourOnes = i2cData & B00001111;
+   HourOnes = i2cData & B00001111;
 
-  //  TH_Not24_flag = bitRead(i2cData, 6);                   // False on RTC when 24 mode selected
-  //  PM_NotAM_flag = bitRead(i2cData, 5);
+   // TH_Not24_flag = bitRead(i2cData, 6); // False on RTC when 24 mode selected
+   // PM_NotAM_flag = bitRead(i2cData, 5);
 
-  if(TH_Not24_flag)
-  {
-    HourTens = i2cData & B00010000;
-    HourTens = HourTens >> 4;
+   if (TH_Not24_flag) {
+      HourTens = i2cData & B00010000;
+      HourTens = HourTens >> 4;
+   } else {
+      HourTens = i2cData & B00110000;
+      HourTens = HourTens >> 4;
+   }
 
-  }
-  else
-  {
-    HourTens = i2cData & B00110000;
-    HourTens = HourTens >> 4;
-  }
+   temphours = HourOnes + (HourTens * 10);// 12 .... 1.2.3...12 or 0 ..1.2.3. ...23
 
-  temphours = HourOnes + (HourTens*10);             // 12 .... 1.2.3...12 or 0 ..1.2.3. ...23
+   if (TH_Not24_flag != new_time_format) {
+      // NewTimeFormat is same format as TH_Not24_flag where H is 12 and LOW is 24
+      if (new_time_format) {
+         // ---------------- 24 -> 12
+         // Convert into 12 hour clock
+         if (temphours >= 12) {
+            PM_NotAM_flag = true; // it is in the PM
 
-  if(TH_Not24_flag != NewTimeFormate)
-  {
-    if(NewTimeFormate)                                    // NewTimeFormate is same formate as TH_Not24_flag where H is 12 and LOW is 24
-    {
-      // ---------------- 24 -> 12
-      // Convert into 12 hour clock
-      if(temphours >= 12)
-      {
-        PM_NotAM_flag = true;                             // it is in the PM
-        if(temphours> 12)
-        {
-          temphours = temphours - 12;                     // Convert from 13:00 .... 23:00 to 1:00 ... 11:00 [Go from 23:59 / 13:00 to 12:00 to 1:00] ?
-        }
-        else
-        {
-          temphours = temphours;                          // do nothing it is 12:00
-        }
+            if (temphours > 12) {
+               /* Convert from 13:00 .... 23:00 to 1:00 ... 11:00
+                  [Go from 23:59 / 13:00 to 12:00 to 1:00] ? */
+               temphours -= 12;
+            } else {
+               temphours = temphours; // do nothing it is 12:00
+            }
+         } else {
+            PM_NotAM_flag = false; // it is in the AM - No other conversion needed
+         }
+
+         if (0 == temphours) {
+            temphours = 12;
+         }
+      } else { // ---------------- 12 -> 24 // Convert into 24 hour clock
+         if ((false == PM_NotAM_flag) && (12 == temphours)) { // AM only check for 00 hours
+            temphours = 0;
+         }
+
+         if (PM_NotAM_flag == true) { // PM conversion
+            if (12 != temphours) { // Leave 12 as 12 in 24h time
+               temphours += 12;
+            }
+         }
       }
-      else
-      {
-        PM_NotAM_flag = false;                            // it is in the AM - No other conversion needed
+
+      // Common finish conversion section
+      TH_Not24_flag = new_time_format;
+
+      HourTens = temphours / 10;
+      HourOnes = temphours % 10;
+
+      // ---
+      temp = (HourTens << 4) + HourOnes;
+
+      if (TH_Not24_flag) {
+         bitWrite(temp, 5, PM_NotAM_flag);
       }
-      if(temphours == 0)
-     {
-       temphours = 12;
-     }
-    }
-    else
-      // ---------------- 12 -> 24                        // Convert into 24 hour clock
-    {
-       if((PM_NotAM_flag == false) && (temphours == 12))  // AM only check for 00 hours
-        {
-          temphours = 0;
-        }
-       if(PM_NotAM_flag == true)
-        {                                                  // PM conversion
-                 if(temphours != 12)                       // Leave 12 as 12 in 24h time
-                 {
-                   temphours = temphours + 12;
-                 }
-        }
-    }
 
-    // Common finish conversion section
-    TH_Not24_flag = new_time_format;
-    HourTens = temphours / 10;
-    HourOnes = temphours % 10;
-
-    // ---
-    temp = (HourTens << 4) + HourOnes;
-    if(TH_Not24_flag)
-    {
-      bitWrite(temp, 5, PM_NotAM_flag);
-    }
-
-    bitWrite(temp, 6, TH_Not24_flag);
-    I2C_TX(RTCDS1337,RTC_HOUR,temp);
-  }
+      bitWrite(temp, 6, TH_Not24_flag);
+      I2C_TX(RTCDS1337, RTC_HOUR, temp);
+   }
 }
