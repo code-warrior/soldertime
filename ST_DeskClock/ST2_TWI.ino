d388a2f4e878f649ce32fe035d345782ba13a310
Roy Vanegas <roy@thecodeeducators.com>
2015-08-25 08:11:04 -0400

    Format function “transmit” and “receive” in file “ST2_TWI.ino”

--- ST_DeskClock/ST2_TWI.ino
+++ ST_DeskClock/ST2_TWI.ino
@@ -1,24 +1,27 @@
-//*******************************************************************************************************************
+//***********************************************************************************
 //                                                          I2C TX RX
-//*******************************************************************************************************************
-void transmit(byte device, byte regadd, byte tx_data)                              // Transmit I2C Data
+//***********************************************************************************
+
+// Transmit I2C Data
+void transmit(byte device, byte regadd, byte tx_data)
 {
-  Wire.beginTransmission(device);
-  Wire.write(regadd);
-  Wire.write(tx_data);
-  Wire.endTransmission();
+   Wire.beginTransmission(device);
+   Wire.write(regadd);
+   Wire.write(tx_data);
+   Wire.endTransmission();
 }
 
-void receive(byte devicerx, byte regaddrx)                                       // Receive I2C Data
+// Receive I2C Data
+void receive(byte devicerx, byte regaddrx)
 {
-  Wire.beginTransmission(devicerx);
-  Wire.write(regaddrx);
-  Wire.endTransmission();
-  Wire.requestFrom(int(devicerx), 1);
+   Wire.beginTransmission(devicerx);
+   Wire.write(regaddrx);
+   Wire.endTransmission();
+   Wire.requestFrom(int(devicerx), 1);
 
-  byte c = 0;
-  if(Wire.available())
-  {
-    data_received_on_i2c = Wire.read();
-  }
+   byte c = 0;
+
+   if (Wire.available()) {
+      data_received_on_i2c = Wire.read();
+   }
 }
